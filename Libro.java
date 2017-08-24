d3e4f8d2aa6120b75dba703b6ed4a306902c0140
Erick <engineererick@hotmail.com>
2017-08-24 11:02:43 -0500

    ...

--- Libro.java
+++ Libro.java
@@ -2,20 +2,27 @@ public class Libro
 {
     private String Titulo = "";
     private String Autor = "";
+    private int Paginas;
     
-    public Libro(String titu, String aut)
-    {
+    public Libro(String titu, String aut, int pag){
         Titulo = titu;
         Autor = aut;
+        Paginas = pag;
     }
     
-    public String DimeAutor()
-    {
+    public String DimeAutor(){
         return Autor;
     }
     
-    public String DimeTitulo()
-    {
+    public String DimeTitulo(){
         return Titulo;
+    }
+    
+    public int DimePaginas(){
+        return Paginas;
+    }
+    
+    public String DimeDetalles(){
+        return "NoLoHeTerminado";
     }
 }
