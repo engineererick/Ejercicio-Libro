a61cfda4846ce2c03ac6ffc6b17b5aed6503352e
Erick <engineererick@hotmail.com>
2017-08-27 21:27:10 -0500

    Todo actualizado, numero de prestamos, referencias

--- Libro.java
+++ Libro.java
@@ -1,28 +1,90 @@
 public class Libro
 {
-    private String Titulo = "";
-    private String Autor = "";
-    private int Paginas;
+    private String titulo;
+    private String autor;
+    private int paginas;
+    private String numRefs;
+    private int prestamos;
     
-    public Libro(String titu, String aut, int pag){
-        Titulo = titu;
-        Autor = aut;
-        Paginas = pag;
+    public Libro(String Titu, String Aut, int nPags)
+    {
+        titulo = Titu;
+        autor = Aut;
+        paginas = nPags;
+        numRefs = "";
+        prestamos = 0;
     }
     
-    public String DimeAutor(){
-        return Autor;
+    public String dimeAutor()
+    {
+        return autor;
     }
     
-    public String DimeTitulo(){
-        return Titulo;
+    public String dimeTitulo()
+    {
+        return titulo;
     }
     
-    public int DimePaginas(){
-        return Paginas;
+    public int dimePaginas()
+    {
+        return paginas;
     }
     
-    public String DimeDetalles(){
-        return "NoLoHeTerminado";
+    public String dimeDetalles()
+    {
+        String cadRes="";
+        cadRes+="Titulo: ";
+        cadRes+=titulo;
+        cadRes+=", ";
+        cadRes+="Autor: ";
+        cadRes+=autor;
+        cadRes+=", ";
+        cadRes+="Paginas: ";
+        cadRes+=paginas;
+        
+        if(numRefs.length()!=0)
+        {
+            cadRes+=",";
+            cadRes+="Numero de Referencias: ";
+            cadRes+=numRefs;
+        }
+        else
+        {
+            cadRes+=",";
+            cadRes+="ZZZ";
+        }
+        cadRes+=", ";
+        cadRes+="Numero de prestamos: ";
+        cadRes+=prestamos;
+        return cadRes;
     }
+    
+    public void cambiaNumRef(String nRef)
+    {
+        if(nRef.length()>2)
+        {
+            numRefs = nRef;
+        }
+        else
+        {
+            nRef = numRefs;
+        }
+    }
+    
+    public String dimeNumRef()
+    {
+        return numRefs;
+    }
+    
+    public void prestar()
+    {
+        prestamos+=1;
+    }
+    
+    public int dimePrestamos()
+    {
+        return prestamos;
+    }
+    
+    
 }
